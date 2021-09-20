0a22e8f0e39524584828321c24d5283cad8bcdbc
JouniKiiskila <43674@edu.raseko.fi>
2021-09-20 12:59:58 +0300

    2

--- kauppa/kauppa/admin.py
+++ kauppa/kauppa/admin.py
@@ -3,6 +3,7 @@ from django.contrib import admin
 from .models import Tuote
 
 class TuoteAdmin(admin.ModelAdmin):
-    fields = ["nimi"]
+    #fields = ["nimi"]
+    fields = ["nimi", "hinta"]
 
 admin.site.register(Tuote, TuoteAdmin)
